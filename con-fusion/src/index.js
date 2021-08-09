6eb0dbb50ad48236c47a31dbeea30d9dd4163bd1
MaxSus <maxsusmail@gmail.com>
2021-08-09 23:53:21 +0200

    react-gtm-module / GTM added

--- con-fusion/src/index.js
+++ con-fusion/src/index.js
@@ -6,7 +6,13 @@ import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import TagManager from 'react-gtm-module'
+const tagManagerArgs = {
+    gtmId: 'GTM-WQZM8ZN'
+}
+ 
+TagManager.initialize(tagManagerArgs)
+ 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
