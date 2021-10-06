b0637072e11ab599c171b7a9b8c518532e95daba
Max <maxsusmail@gmail.com>
2021-10-06 19:15:22 +0200

    added react-piwik plugin but not initialized yet, need to research documentation further

--- con-fusion/src/index.js
+++ con-fusion/src/index.js
@@ -7,11 +7,19 @@ import 'bootstrap-social/bootstrap-social.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import TagManager from 'react-gtm-module'
+import ReactPiwik from 'react-piwik';
+
 const tagManagerArgs = {
     gtmId: 'GTM-WQZM8ZN'
 }
- 
 TagManager.initialize(tagManagerArgs)
+
+const piwik = new ReactPiwik({
+    url: 'salesdemo.piwik.pro',
+    siteId: 'abf885dc-e088-48a4-a2a7-bd4a264cf082',
+  });
+   
+  const trackAtConnect = false;
  
 ReactDOM.render(<App />, document.getElementById('root'));
 
